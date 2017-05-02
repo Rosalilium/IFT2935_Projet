bf57cd814e2957819e34b9cd52056b6813adaca5
Amelie <ame.lacombe.r@gmail.com>
2017-05-02 12:47:53 -0400

    Added driver and sample code to project

--- Projet_Eclipse/IFT2935_Projet/src/sql/SQLHelper.java
+++ Projet_Eclipse/IFT2935_Projet/src/sql/SQLHelper.java
@@ -1,5 +1,8 @@
 package sql;
 
+import oracle.jdbc.driver.OracleDriver;
+import java.sql.*;
+
 // Mettre dans ce package tout ce qui est en lien avec le SQL. (connection au serveur,
 // requêtes, etc.)
 // Le nom de la classe peut être modifier c'était juste pour avoir quelque chose
@@ -10,5 +13,55 @@ package sql;
 // dans le package "sql".
 
 public class SQLHelper {
+	
+	final static String URL = "jdbc:oracle:thin:@oracle.iro.umontreal.ca:1521:orcl";
+	final static String USER = "lacombam";
+	final static String PW = "bamp108L";
+	
+	Connection connection;
+	
+	public void connect() {
+		
+	}
+	
+	ResultSet query(String query) {
+		
+		return null;
+	}
 
+	/*public static void main(String[] args) {
+		
+		Connection connection = null;
+		
+		try {
+			connection = DriverManager.getConnection(URL, USER, PW);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		if (connection != null) {
+			System.out.println("Connection successful!");
+			
+			Statement statement = null;
+			
+			String query = "SELECT table_name FROM user_tables";
+			
+			try {
+				statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery(query);
+				while(resultSet.next()) {
+					System.out.println(resultSet.getString(1));
+					System.out.println();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+		
+			
+			}
+		} else {
+			System.out.println("Erreur");
+			System.exit(-1);
+		}
+	}*/
 }
